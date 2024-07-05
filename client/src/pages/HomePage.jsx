ba9d01a2522710819d5acf614ec218ecc77023af
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2024-07-05 07:50:51 +0600

    Proxy Settings Done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react"
+import Axios from "axios"
 import MainLayout from "../components/MainLayout"
 
 const HomePage = () => {
+
+  const getAllItems = () => {
+    Axios.get('/api/v1/items/get-all-items').then((response) => {
+      console.log(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    getAllItems()
+  }, [])
+
   return (
     <MainLayout>
       <h1>HomePage</h1>
