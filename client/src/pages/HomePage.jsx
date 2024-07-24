a614d9e887ac267b66b34561c7d8e5d36613d0d8
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2024-07-24 21:24:57 +0600

    Category Function done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,7 +8,24 @@ import Item from "../components/Item";
 
 const HomePage = () => {
   const [itemsData, setItemsData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("ready mix");
   const dispatch = useDispatch();
+
+  const categories = [
+    {
+      name: "ready mix",
+      imageURL: "https://www.sfbl.com.bd/uploaded_files/300tn_thumb_Chotpoti.PNG"
+    },
+    {
+      name: "Pulses & Cereals",
+      imageURL: "https://www.sfbl.com.bd/uploaded_files/300tn_thumb_Kheer%20Pack%20Right.jpg"
+    },
+    {
+      name: "besic spices",
+      imageURL: "https://www.sfbl.com.bd/uploaded_files/300tn_thumb_turmeric(1).png"
+    },
+  ];
+
 
   const getAllItems = () => {
     dispatch({ type: "showLoading" });
@@ -16,9 +33,9 @@ const HomePage = () => {
       setItemsData(response.data);
       dispatch({ type: "hideLoading" });
     }).catch((error) => {
-      dispatch({ type: "hideLoading" }); 
+      dispatch({ type: "hideLoading" });
       console.log(error);
-    }); 
+    });
   };
 
   useEffect(() => {
@@ -27,8 +44,18 @@ const HomePage = () => {
 
   return (
     <MainLayout>
+      <div className="category">
+        {categories.map((category) => {
+          return (
+            <div onClick={() => setSelectedCategory(category.name)} className={`${selectedCategory === category.name && 'selected-category'}`}>
+              <h4>{category.name}</h4>
+              <img src={category.imageURL} height="60" width="60" />
+            </div>
+          );
+        })}
+      </div>
       <Row>
-        {itemsData.map((item) => {
+        {itemsData.filter((i) => i.category === selectedCategory).map((item) => {
           return (
             <Col xs={24} lg={6} md={12} sm={6}>
               <Item item={item} />
