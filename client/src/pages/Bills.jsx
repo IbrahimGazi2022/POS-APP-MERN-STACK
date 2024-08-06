19418df5369e8d891e186f0a71720bb10e525529
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2024-08-07 00:02:33 +0600

    Bill Receipt Details Complete

--- client/src/pages/Bills.jsx
+++ client/src/pages/Bills.jsx
@@ -3,10 +3,14 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import { EyeOutlined } from "@ant-design/icons";
-import { Table, Button } from 'antd';
+import { Table } from 'antd';
+import Modal from 'antd/es/modal/Modal';
+import "../resources/bill.css";
 
 const Bills = () => {
     const [billsData, setBillsData] = useState([]);
+    const [printBillModalVisibility, setPrintBillModalVisibility] = useState(false);
+    const [selectedBill, setSelectedBill] = useState(null);
     const dispatch = useDispatch();
 
     const getAllBills = () => {
@@ -52,12 +56,44 @@ const Bills = () => {
             dataIndex: "_id",
             render: (id, record) => (
                 <div className="d-flex">
-                    <EyeOutlined />
+                    <EyeOutlined onClick={() => {
+                        setPrintBillModalVisibility(true);
+                        setSelectedBill(record);
+                    }} />
                 </div>
             ),
         },
     ];
 
+
+    const cartcolumns = [
+        {
+            title: "Name",
+            dataIndex: "name",
+        },
+        {
+            title: "Price",
+            dataIndex: "price",
+        },
+        {
+            title: "Quantity",
+            dataIndex: "_id",
+            render: (id, record) => (
+                <div>
+                    <b>{record.quantity}</b>
+                </div>
+            ),
+        },
+        {
+            title: "Total fare",
+            dataIndex: "_id",
+            render: (id, record) => (
+                <div>
+                    <b>{record.quantity * record.price}</b>
+                </div>
+            ),
+        },
+    ];
 
     useEffect(() => {
         getAllBills();
@@ -69,7 +105,31 @@ const Bills = () => {
             <div className='item-section'>
                 <h1>All Bills</h1>
             </div>
-            <Table dataSource={billsData} columns={columns} bordered></Table>   
+            <Table dataSource={billsData} columns={columns} bordered></Table>
+            {printBillModalVisibility && (
+                <Modal title="Bill Details" open={printBillModalVisibility} onCancel={() => { setPrintBillModalVisibility(false); }} footer={false}>
+                    <div>
+                        <div className='invoice'>
+                            <div>
+                                <h1><b>Store Controller</b></h1>
+                            </div>
+                        </div>
+                        <div className='bill-customer'>
+                            <p><b>Name : </b>{selectedBill.customerName}</p>
+                            <p><b>Phone Number : </b>{selectedBill.customerPhoneNumber}</p>
+                            <p><b>Date</b> :{selectedBill.createdAt.toString().substring(0, 10)}</p>
+                        </div>
+
+                        <Table dataSource={selectedBill.cartItems} columns={cartcolumns} pagination={false} />
+
+                        <div className="dotted-border">
+                            <p><b>SUB TOTAL</b> : {selectedBill.subTotal}</p>
+                            <p><b>Tax</b> : {selectedBill.tax}</p>
+                        </div>
+
+                    </div>
+                </Modal>
+            )}
         </MainLayout >
     );
 };
